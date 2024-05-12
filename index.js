6aa0ade2c09e598b44d9704526c7541384f7e7b5
edwfguhsaij <gs1meza0097@hotmail.com>
2024-05-12 21:10:40 +0700

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const dropWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
-  return arr;
-};
+const firstName = "John";
+const lastName = "Doe";
+const fullName = firstName + " " + lastName;
+console.log(fullName); // John Doe
