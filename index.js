4d4741efcb1eef7066776169c541c6b11d1dc0fc
edwfguhsaij <gs1meza0097@hotmail.com>
2024-05-14 04:21:55 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-const firstName = "John";
-const lastName = "Doe";
-const fullName = firstName + " " + lastName;
-console.log(fullName); // John Doe
+const cycleSort = (arr) => {
+  for (let start = 0; start < arr.length - 1; start++) {
+    let item = arr[start];
+    let pos = start;
+    for (let i = start + 1; i < arr.length; i++) {
+      if (arr[i] < item) {
+        pos++;
+      }
+    }
+    if (pos === start) {
+      continue;
+    }
+    while (item === arr[pos]) {
+      pos++;
+    }
+    if (pos !== start) {
+      [item, arr[pos]] = [arr[pos], item];
+    }
+    while (pos !== start) {
+      pos = start;
+      for (let i = start + 1; i < arr.length; i++) {
+        if (arr[i] < item) {
+          pos++;
+        }
+      }
+      while (item === arr[pos]) {
+        pos++;
+      }
+      if (item !== arr[pos]) {
+        [item, arr[pos]] = [arr[pos], item];
+      }
+    }
+  }
+  return arr;
+};
